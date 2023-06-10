e54d9de68fc8fb8f231aedb8cfe148841d20f318
guidowilliam11 <guido.william@binus.ac.id>
2023-06-10 23:35:02 +0700

    feats : thread seeder

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Comment;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 class CommentSeeder extends Seeder
 {
@@ -20,68 +22,70 @@ class CommentSeeder extends Seeder
             'message' => 'Yes, Eco enzyme enhances soil fertility, structure, and crop yield. It breaks down organic matter, boosts nutrient availability, and encourages beneficial microbes for healthy plant growth.',
             'posting_date' => '2023-03-06 14:25:43'
         ]);
-        
+
         Comment::create([
             'user_id' => 2,
             'thread_id' => 5,
             'message' => 'Eco enzyme enhances soil fertility, structure, and crop yield. It breaks down organic matter, boosts nutrient availability, and encourages beneficial microbes for healthy plant growth.',
             'posting_date' => '2023-03-07 06:23:18'
         ]);
-            
+
         Comment::create([
             'user_id' => 3,
             'thread_id' => 5,
             'message' => 'Yes, eco enzyme has shown potential for enhancing crop yield and soil health. It aids organic matter decomposition, nutrient availability, soil structure improvement.',
             'posting_date' => '2023-03-08 03:54:12'
         ]);
-            
+
         Comment::create([
             'user_id' => 1,
             'thread_id' => 3,
             'message' => 'Eco enzyme is eco-friendly and biodegradable unlike harsh chemical cleaners. Conventional cleaners harm aquatic life and the environment, while eco enzyme derived from natural sources poses minimal risks.',
             'posting_date' => '2023-03-06 06:56:18'
         ]);
-            
+
         Comment::create([
             'user_id' => 2,
             'thread_id' => 3,
             'message' => 'Eco enzyme is safer for human health compared to chemical cleaners containing irritants or hazardous substances. It\'s a preferred choice for individuals with sensitivities, households with children/pets, or those seeking to minimize exposure.',
             'posting_date' => '2023-03-06 07:12:12'
         ]);
-            
+
         Comment::create([
             'user_id' => 3,
             'thread_id' => 3,
             'message' => 'Eco enzymes offer sustainable and cost-effective cleaning solutions. They use waste materials, reducing reliance on virgin resources. Eco enzyme production is simple and can save money. Help minimize carbon footprint.',
             'posting_date' => '2023-03-06 23:52:15'
         ]);
-            
+
         Comment::create([
             'user_id' => 1,
             'thread_id' => 4,
             'message' => 'Eco enzymes are sensitive to extreme temperatures and pH levels. Store them properly. Avoid mixing with strong acids or bases. Maintain stability and effectiveness.',
             'posting_date' => '2023-03-06 06:56:18'
         ]);
-            
+
         Comment::create([
             'user_id' => 2,
             'thread_id' => 4,
             'message' => 'Follow dilution ratios and application guidelines for optimal results and to avoid damage. Proper application ensures performance.',
             'posting_date' => '2023-03-06 12:23:34'
         ]);
-            
+
         Comment::create([
             'user_id' => 3,
             'thread_id' => 4,
             'message' => 'Eco enzymes may have a distinct odor. Ensure ventilation during application. Consider personal sensitivities and preferences.',
             'posting_date' => '2023-03-06 15:23:34'
         ]);
-            
+
         Comment::create([
             'user_id' => 4,
             'thread_id' => 4,
             'message' => 'Check compatibility with specific materials before use. Some surfaces may require specialized cleaning methods. Perform a patch test for compatibility.',
             'posting_date' => '2023-03-06 12:12:15'
-        ]);        
+        ]);
+
+
     }
 }
--- database/seeders/ThreadSeeder.php
+++ database/seeders/ThreadSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Thread;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 class ThreadSeeder extends Seeder
 {
@@ -34,26 +36,114 @@ class ThreadSeeder extends Seeder
             'message' => 'What are the advantages of eco enzyme compares to conventional chemical',
             'posting_date' => '2023-05-12 23:23:15'
         ]);
-        
+
         Thread::create([
             'user_id' => 4,
             'title' => 'Limitation of Eco enzyme',
             'message' => 'While eco enzyme has many benefits is there is any limitation or precautions of using eco enzyme?',
             'posting_date' => '2023-05-02 15:12:42'
         ]);
-        
+
         Thread::create([
             'user_id' => 5,
             'title' => 'Eco Enzyme for Agricultural',
             'message' => 'Can eco enzyme be used for agricultural purposes, such as improving crop yield or soil health?',
             'posting_date' => '2023-05-04 16:57:52'
         ]);
-        
+
         Thread::create([
             'user_id' => 6,
             'title' => 'Variation Eco-Enzyme',
             'message' => 'Are there any variations or different types of eco enzyme available, and what are their specific uses?',
             'posting_date' => '2023-03-06 06:56:18'
         ]);
+
+        $faker = Faker::create();
+
+        $titleList = [
+            "Benefits of Using Eco-Enzyme",
+            "How to Make Eco-Enzyme at Home?",
+            "Eco-Enzyme for Cleaning Purposes",
+            "Eco-Enzyme as a Natural Fertilizer",
+            "The Science Behind Eco-Enzyme",
+            "Eco-Enzyme: A Sustainable Solution",
+            "Eco-Enzyme vs. Chemical Cleaners: A Comparison",
+            "Eco-Enzyme: An Eco-Friendly Alternative",
+            "Using Eco-Enzyme for Waste Management",
+            "The Role of Enzymes in Eco-Enzyme",
+            "Eco-Enzyme and its Impact on Soil Health",
+            "Eco-Enzyme for Odor Elimination",
+            "Eco-Enzyme and Water Conservation",
+            "Eco-Enzyme: A Natural Pest Repellent",
+            "The Versatility of Eco-Enzyme",
+            "Eco-Enzyme: An Affordable Solution",
+            "Eco-Enzyme and Sustainable Agriculture",
+            "Eco-Enzyme for Septic Tank Maintenance",
+            "Eco-Enzyme and Household Cleaning",
+            "Eco-Enzyme and Organic Waste Decomposition",
+            "Eco-Enzyme: A Renewable Resource",
+            "Eco-Enzyme and Composting",
+            "Eco-Enzyme: Safe for Pets and Children?",
+            "Eco-Enzyme and Grease Removal",
+            "Eco-Enzyme: A Non-Toxic Alternative",
+            "Eco-Enzyme and Industrial Applications",
+            "Eco-Enzyme: Reducing Environmental Footprint",
+            "Eco-Enzyme and Stain Removal",
+            "Eco-Enzyme: A Green Solution for Laundry",
+            "Eco-Enzyme: Benefits for the Home Garden",
+        ];
+
+        $messageList = [
+            "What are the benefits of using eco-enzyme?",
+            "Can you provide a step-by-step guide on making eco-enzyme at home?",
+            "How can eco-enzyme be used for cleaning purposes?",
+            "What are the advantages of using eco-enzyme as a natural fertilizer?",
+            "What is the scientific explanation behind eco-enzyme?",
+            "How does eco-enzyme contribute to sustainability?",
+            "What are the differences between eco-enzyme and chemical cleaners?",
+            "How eco-friendly is eco-enzyme compared to other alternatives?",
+            "In what ways can eco-enzyme be used for waste management?",
+            "What is the role of enzymes in eco-enzyme production?",
+            "How does eco-enzyme affect soil health?",
+            "Can eco-enzyme effectively eliminate odors?",
+            "How does eco-enzyme contribute to water conservation?",
+            "Is eco-enzyme a natural repellent for pests?",
+            "What are the different applications of eco-enzyme?",
+            "Is eco-enzyme an affordable solution for everyday use?",
+            "How does eco-enzyme support sustainable agriculture practices?",
+            "How can eco-enzyme be used for septic tank maintenance?",
+            "Can eco-enzyme be used for various household cleaning tasks?",
+            "How does eco-enzyme facilitate organic waste decomposition?",
+            "Is eco-enzyme a renewable resource?",
+            "How does eco-enzyme contribute to composting processes?",
+            "Is eco-enzyme safe for pets and children?",
+            "How effective is eco-enzyme in removing grease?",
+            "Is eco-enzyme a non-toxic alternative to conventional cleaners?",
+            "What are the industrial applications of eco-enzyme?",
+            "How does eco-enzyme help reduce environmental footprint?",
+            "Can eco-enzyme effectively remove stains?",
+            "How does eco-enzyme provide a green solution for laundry?",
+            "What are the benefits of using eco-enzyme in home gardening?",
+        ];
+
+        $data = [];
+
+        for ($index = 0; $index < count($titleList); $index++) {
+            $userId = $faker->numberBetween(1, 6);
+            $title = $titleList[$index];
+            $message = $messageList[$index];
+            $postingDate = $faker->dateTimeBetween('2023-04-01', '2023-06-10');
+            $createdAt = Carbon::parse($postingDate)->subSeconds(1);
+            $updatedAt = $createdAt;
+
+            Thread::create([
+                'user_id' => $userId,
+                'title' => $title,
+                'message' => $message,
+                'posting_date' => $postingDate,
+                'created_at' => $createdAt,
+                'updated_at' => $updatedAt,
+            ]);
+        }
     }
 }
